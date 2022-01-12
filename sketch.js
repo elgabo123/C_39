eeb243d826c953abfff088a1188bb9d1c2bcbe5a
elgabo123 <90482941+elgabo123@users.noreply.github.com>
2022-01-11 18:04:33 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+var car1, car2, car3, car4, cars;
+var form, player, game;
+var track, car1Img, car2Img, car3Img, car4Img,groundimg;
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1Img=loadImage("images/car1");
+  car2Img=loadImage("images/car2");
+  car3Img=loadImage("images/car3");
+  car4Img=loadImage("images/car4");
+  groundimg = loadImage("images/ground.png");
+}
+function setup(){
+  canvas = createCanvas(displayWidth-20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
